9a2c7e8ffede200399ff24b0da17427da098f232
bhatiaheman <bhatiaheman8@gmail.com>
2024-01-29 23:31:37 +0530

    Refactor ReactRouter components and add routing

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+
 
 
 function App() {
@@ -8,7 +10,7 @@ function App() {
 
   return (
     <>
-      <h1>React Router</h1>
+      
     </>
   )
 }
--- ReactRouter/src/main.jsx
+++ ReactRouter/src/main.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import User from './components/User/User'
+
+//const router = createBrowserRouter([
+//  {
+  //  path: '/',
+ //   element: <Layout />,
+  //  children: [
+      
+ //   ]
+ // }
+//])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path='' element={<Home />}/>
+      <Route path='user/:id' element={<User />}/>
+
+    </Route>
+  )
+  
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
