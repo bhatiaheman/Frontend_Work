499989365bd7e1bba8d7b7447315f5d1c0447460
bhatiaheman <bhatiaheman8@gmail.com>
2024-07-30 12:52:31 +0530

    custom input component

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -1,14 +1,43 @@
+/* eslint-disable react/jsx-no-undef */
 
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
 import './App.css'
+import authService from './appwrite/auth';
+import {login, logout} from "./store/authSlice";
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
-  console.log(import.meta.env.VITE_APPRWRITE_URL)
 
-  return (
-    <>
-      <h1>Blog app</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        
+        if(userData) {
+          dispatch(login({userData}))
+        } else {
+          dispatch(logout())
+        }
+
+      })
+      .finally(() => setLoading(false))                    // finally always run
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+          <main>
+            
+          </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
